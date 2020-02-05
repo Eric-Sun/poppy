cf262acc1f4b34299696e9e5e759c52382874329
sunbo <docsun.bo@gmail.com>
2020-02-05 21:49:30 +0800

    add

--- src/main/java/com/j13/poppy/starter/SpringBootStarter.java
+++ src/main/java/com/j13/poppy/starter/SpringBootStarter.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 
 @EnableAutoConfiguration
 @ImportResource({"classpath:applicationContext.xml"})
+@PropertySource({"classpath:application.properties"})
 @SpringBootApplication
 public class SpringBootStarter {
 
